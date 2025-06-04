d0d74498300b84c93edc53e2160374262e8f3bfd
muumz <useless.muumz@gmail.com>
2025-06-04 12:26:45 +0200

    Encryption fixed.

--- softdesk/users/serializers.py
+++ softdesk/users/serializers.py
@@ -29,3 +29,6 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', "password", 'date_of_birth', 'can_be_contacted', 'can_data_be_shared', "is_active", "is_staff", "is_superuser"]
         read_only_fields = ["id", "is_superuser", "is_staff", "is_active"]
 
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
